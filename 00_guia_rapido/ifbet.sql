1204669133c6a660c4efe99794395c8d3f488e59
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2026-08-13 20:12:41 -0300

    atualizando

--- 00_guia_rapido/ifbet.sql
+++ 00_guia_rapido/ifbet.sql
@@ -104,5 +104,162 @@ BEGIN
  END;
 $$ LANGUAGE 'plpgsql';
 
+-- 1
+CREATE OR REPLACE FUNCTION obter_saldo(var_usuario_id integer) RETURNS money AS
+$$
+DECLARE 
+    saldo_atual money := 0::money;
+BEGIN
+    SELECT saldo FROM usuario WHERE id = var_usuario_id INTO saldo_atual;
+    RETURN saldo_atual;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- 2
+CREATE OR REPLACE FUNCTION quantidade_apostas(var_usuario_id integer) RETURNS integer AS
+$$
+DECLARE 
+    qtde_apostas integer := 0;
+BEGIN
+    SELECT count(*) FROM aposta WHERE usuario_id = var_usuario_id INTO qtde_apostas;
+    RETURN qtde_apostas;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- 3
+CREATE OR REPLACE FUNCTION valor_total_apostado(var_usuario_id integer) RETURNS money AS
+$$
+DECLARE 
+    valor_total money := 0::money;
+BEGIN
+    SELECT sum(valor) FROM aposta WHERE usuario_id = var_usuario_id INTO valor_total;
+    RETURN valor_total;
+END;
+$$ LANGUAGE 'plpgsql';
+
+
+-- cuidar: como saber se ganhou e qto ganhou? odd * valor_aposta = mas neste, devemos verificar se o resultado bate
+
+-- 4
+CREATE OR REPLACE FUNCTION nome_equipe(var_equipe_id integer) RETURNS text AS 
+$$
+DECLARE
+    resultado text;
+BEGIN
+    SELECT nome FROM equipe WHERE id = var_equipe_id INTO resultado;
+    RETURN resultado;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- 5
+CREATE OR REPLACE FUNCTION nome_jogo(var_jogo_id integer) RETURNS text AS
+$$
+DECLARE
+    resultado_casa text;
+    resultado_visitante text;
+BEGIN
+    SELECT nome FROM equipe JOIN jogo ON equipe.id = jogo.equipe_casa_id WHERE jogo.id = var_jogo_id INTO resultado_casa;
+    
+    SELECT nome FROM equipe JOIN jogo ON equipe.id = jogo.equipe_visitante_id WHERE jogo.id = var_jogo_id INTO resultado_visitante;
+    
+    RETURN resultado_casa || ' x ' || resultado_visitante;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- 6
+CREATE OR REPLACE FUNCTION possui_saldo(var_usuario_id integer) RETURNS BOOLEAN AS
+$$
+DECLARE
+    saldo_atual money := 0::money;
+BEGIN
+    saldo_atual := obter_saldo(var_usuario_id);
+    IF (saldo_atual IS NULL OR saldo_atual <= 0::money) THEN
+        RETURN FALSE;
+    END IF;
+    RETURN TRUE;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- 7
+CREATE OR REPLACE FUNCTION total_usuarios() RETURNS INTEGER AS
+$$
+DECLARE
+    total integer := 0;
+BEGIN
+    select count(*) FROM usuario INTO total;
+    RETURN total;
+END;
+$$ LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION total_equipes() RETURNS INTEGER AS
+$$
+DECLARE
+    total integer := 0;
+BEGIN
+    select count(*) FROM equipe INTO total;
+    RETURN total;
+END;
+$$ LANGUAGE 'plpgsql';
+
+
+
+CREATE OR REPLACE FUNCTION total_jogos() RETURNS INTEGER AS
+$$
+DECLARE
+    total integer := 0;
+BEGIN
+    select count(*) FROM jogo INTO total;
+    RETURN total;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- 10
+
+CREATE OR REPLACE FUNCTION media_valor_apostas() RETURNS money AS
+$$
+DECLARE
+    media money := 0::money;
+BEGIN
+    select avg(valor::numeric) FROM aposta INTO media;
+    RETURN media;
+END;
+$$ LANGUAGE 'plpgsql';
+
+
+-- 11
+CREATE OR REPLACE FUNCTION usuario_mais_rico() RETURNS money AS
+$$
+DECLARE
+    maior_saldo money := 0::money;
+BEGIN
+    select MAX(saldo) FROM usuario INTO maior_saldo;
+    RETURN maior_saldo;
+END;
+$$ LANGUAGE 'plpgsql';
+
+
+-- 12
+CREATE OR REPLACE FUNCTION maior_aposta() RETURNS money AS
+$$
+DECLARE
+    maior money := 0::money;
+BEGIN
+    select MAX(valor) FROM aposta INTO maior;
+    RETURN maior;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- 13
+CREATE OR REPLACE FUNCTION apostas_usuario(var_usuario_id integer) RETURNS 
+    TABLE (var_id integer, var_valor money, var_odd real) AS
+$$
+DECLARE
+BEGIN
+    RETURN QUERY SELECT id, valor, odd FROM aposta WHERE usuario_id = var_usuario_id; 
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- select * from apostas_usuario(1);
 
 
